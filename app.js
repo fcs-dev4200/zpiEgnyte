ed6e89d725edccf85d67d280f18578deb1289e68
fcs-dev4200 <developer@fcskc.com>
2026-03-19 17:02:11 -0500

    disabled ngrok

--- app.js
+++ app.js
@@ -113,11 +113,11 @@ process.on("unhandledRejection", (err) => {
 
 // This should be left disable during production. Only enable this if you are doing local development
 
-(async function () {
-  const listener = await ngrok.forward({
-    addr: port,
-    authtoken: process.env.NGROK_AUTHTOKEN,
-    domain: process.env.NGROK_DOMAIN,
-  });
-  console.log("Ingress established at", listener.url());
-})();
+// (async function () {
+//   const listener = await ngrok.forward({
+//     addr: port,
+//     authtoken: process.env.NGROK_AUTHTOKEN,
+//     domain: process.env.NGROK_DOMAIN,
+//   });
+//   console.log("Ingress established at", listener.url());
+// })();
